01e349158b5fba4d9255c0d33a8fbcb47037e010
hazel <hazel.madolid@gmail.com>
2022-05-22 11:34:42 -0500

    added support for ropsten testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,14 @@
+const dotenv = require("dotenv");
+dotenv.config({path: __dirname + '/.env'});
+const { INFURA_URL, ROPSTEN_PRIVATE_KEY } = process.env;
 require("@nomiclabs/hardhat-waffle");
 
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    ropsten: {
+      url: INFURA_URL,
+      accounts: [`0x${ROPSTEN_PRIVATE_KEY}`]
+    }
+  }
 };
